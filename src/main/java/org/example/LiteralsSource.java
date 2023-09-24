1917c64fbabbc1d3a8e4fc71328c0edc9a9ef0d7
Nazar Holovenko <nazargolovenko336@gmail.com>
2023-09-24 23:09:06 +0300

    LiteralsSource class is final from this commit. added char array of English lowercase literals

--- src/main/java/org/example/LiteralsSource.java
+++ src/main/java/org/example/LiteralsSource.java
@@ -1,4 +1,8 @@
 package org.example;
 
 public final class LiteralsSource {
+    public static final char[] ENGLISH_LOWERCASE_ALPHABET =
+            {'a','b','c','d','e','f','g','h',
+            'i','j','k','l','m','n','o','p','q','r','s',
+            't','u','v','w','x','y','z'};
 }
