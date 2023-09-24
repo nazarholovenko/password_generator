c82325d6b67f633f9d893f6ba8f34d5088db9886
Nazar Holovenko <nazargolovenko336@gmail.com>
2023-09-24 23:10:47 +0300

    added char array of english uppercase literals

--- src/main/java/org/example/LiteralsSource.java
+++ src/main/java/org/example/LiteralsSource.java
@@ -5,4 +5,8 @@ public final class LiteralsSource {
             {'a','b','c','d','e','f','g','h',
             'i','j','k','l','m','n','o','p','q','r','s',
             't','u','v','w','x','y','z'};
+    public static final char[] ENGLISH_UPPERCASE_ALPHABET =
+            {'A','B','C','D','E','F','G','H','I',
+            'J','K','L','M','N','O','P','Q','R', 'S',
+                    'T','U','V','W','X','Y','Z'};
 }
